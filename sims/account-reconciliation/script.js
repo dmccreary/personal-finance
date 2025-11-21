a7ef7254cf1e1cf12524ede56af92a71f95a438c
Dan McCreary <dan.mccreary@gmail.com>
2025-11-20 18:05:52 -0600

    Deployed 04f75ea with MkDocs version: 1.6.1

---
+++ sims/account-reconciliation/script.js
@@ -0,0 +1,5 @@
+// Account Reconciliation Process - External Scripts
+// Note: Main functionality is inline in main.html for iframe compatibility
+
+// This file can be used for additional enhancements if needed
+console.log('Account Reconciliation Process loaded');
